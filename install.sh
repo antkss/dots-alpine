b0b243b6501e54215a0812353980e76dd1071293
lmaodark <lmao@gmail.com>
2025-05-12 10:01:35 +0000

    initialize2

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sysinit=(udev-trigger udev-settle udev sysfs procfs iwd fsck devfs)
 default=(brightnessctl dbus dhcpcd hostname hwclock loopback mdevd networkmanager polkit seatd udev-postmount elogind)
 install_initial() {
-	apk add bash sudo openrc eudev mdevd networkmanager iwd seatd elogind dhcpcd brightnessctl git pulseaudio
+	apk add bash sudo openrc eudev mdevd networkmanager iwd seatd elogind dhcpcd brightnessctl git pulseaudio polkit
 }
 install_service() {
 	for i in ${sysinit[@]}; do
@@ -11,8 +11,17 @@ install_service() {
 		rc-update add $i default
 	done
 }
+install_browser() {
+	apk add firefox
+	npm install sass -g
+}
+install_core() {
+	apk add linux-lts linux-lts-dev intel-ucode linux-firmware sof-firmware
+}
 install_initial
 ./source.sh
 bash ./package.sh
+install_browser
 install_service
+install_core
 
